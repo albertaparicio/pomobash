1150a24190721d3779a9f7e0af58292122bb9a88
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2016-02-21 20:51:43 +0100

    Issue #8 - Specify parameters as options in command line
    Now the user can enter the program's parameters in the command line, or enter the interactive mode if no arguments are passed.
    
    The lines corresponding to the interactive mode, and the ones that run the pomos are now organized in functions.

--- pomobash.sh
+++ pomobash.sh
@@ -2,31 +2,40 @@
 
 # Terminal-based Pomodoro Technique chronometer
 
-# Number of pomodoros in each cycle
-echo "Introduce desired Pomo count: "
-read POMO_NUM
+interactive_mode()
+{
 
-# Pomodoro time (in minutes)
-#POMO=25
-echo "Introduce desired Pomodoro time (in minutes): "
-read POMO_TIME
-#POMO_M=$(( $POMO_TIME*60 ))
-POMO_M=$POMO_TIME
+	# Number of pomodoros in each cycle
+	echo "Introduce desired Pomo count: "
+	read POMO_NUM
 
-# In-between-pomo time (in minutes)
-#BREAK=5
-echo "Introduce desired short break time (in minutes): "
-read BREAK
-#BREAK_M=$(( $BREAK*60 ))
-BREAK_M=$BREAK
+	# Pomodoro time (in minutes)
+	#POMO=25
+	echo "Introduce desired Pomodoro time (in minutes): "
+	read POMO_TIME
+	#POMO_M=$(( $POMO_TIME*60 ))
+	POMO_M=$POMO_TIME
 
-# Long break time (in minutes)
-#LBREAK=15
-echo "Introduce desired long break time (in minutes): "
-read LBREAK
-#LBREAK_M=$(( $LBREAK*60 ))
-LBREAK_M=$LBREAK
+	# In-between-pomo time (in minutes)
+	#BREAK=5
+	echo "Introduce desired short break time (in minutes): "
+	read BREAK
+	#BREAK_M=$(( $BREAK*60 ))
+	BREAK_M=$BREAK
 
+	# Long break time (in minutes)
+	#LBREAK=15
+	echo "Introduce desired long break time (in minutes): "
+	read LBREAK
+	#LBREAK_M=$(( $LBREAK*60 ))
+	LBREAK_M=$LBREAK
+
+	run_pomo_cycle && exit 0
+
+}
+
+run_pomo_cycle()
+{
 # Pomodoro in action, in sets of the user input Pomodoros
 echo "Pomodoro running...
 "
@@ -35,6 +44,46 @@ do
 	sleep $POMO_M && notify-send "break" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga; sleep $BREAK_M && notify-send "back to work" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga
 done
 
-#sleep $POMO_M && notify-send "break" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga; sleep $BREAK_M && notify-send "back to work" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga
-#sleep $POMO_M && notify-send "break" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga; sleep $BREAK_M && notify-send "back to work" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga
 sleep $POMO_M && notify-send "long break" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga; sleep $LBREAK_M && notify-send "back to work" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga
+
+exit 0
+}
+
+
+POMO_NUM=
+POMO_M=
+BREAK_M=
+LBREAK_M=
+
+[ $# -eq 0 ] && { interactive_mode; exit 0; }
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --pomo-number )    shift
+                                POMO_NUM=$1
+                                ;;
+        -t | --pomo-time )    	shift
+				POMO_M=$1
+                                ;;
+        -s | --short-break-time )    	shift
+				BREAK_M=$1
+                                ;;
+        -l | --long-break-time )    	shift
+				LBREAK_M=$1
+                                ;;
+        #-h | --help )           get_help
+        #                        exit 0
+        #                        ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+
+run_pomo_cycle && exit 0
+
+
+#sleep $POMO_M && notify-send "break" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga; sleep $BREAK_M && notify-send "back to work" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga
+#sleep $POMO_M && notify-send "break" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga; sleep $BREAK_M && notify-send "back to work" && mplayer --really-quiet /usr/share/sounds/freedesktop/stereo/complete.oga
+
